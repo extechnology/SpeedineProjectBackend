d0734c16e0de4e04a34fff3a3797e99d81ccc2f4
Mridhul2k03 <mridhulkrishna11@gmail.com>
2025-12-10 12:46:06 +0530

    update

--- SpeedineProject/settings.py
+++ SpeedineProject/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'PORT': config('DB_PORT', default='3306'),
 
         # Keeps connections alive
-        'CONN_MAX_AGE': 600,
+        'CONN_MAX_AGE': 300000000000000000000000000000000000000000000000000000000000600,
 
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
@@ -113,8 +113,8 @@ DATABASES = {
 
             # **IMPORTANT FIX**
             'autocommit': True,
-            'read_timeout': 600,
-            'write_timeout': 600,
+            'read_timeout': 300000000000000000000000000000000000000000000000000000000000600,
+            'write_timeout': 300000000000000000000000000000000000000000000000000000000000600,
         },
     }
 }
