507892a862bf0a1d32660c04f1391a9c682475de
Mridhul2k03 <mridhulkrishna11@gmail.com>
2025-12-17 11:28:35 +0530

    ur;ls

--- Dashboard/UserInformations/user_urls.py
+++ Dashboard/UserInformations/user_urls.py
@@ -3,4 +3,5 @@ from .user_views import *
 
 urlpatterns = [
     path("users", UserListView.as_view(), name="user-list"),
+    path("contacts", ContactListView.as_view(), name="contact-list"),   
 ]
