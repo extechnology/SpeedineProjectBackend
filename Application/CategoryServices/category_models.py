1f3eba523c403b4269f59054acae7eb2d51d07ed
VIPINRAJK26 <vipinrajk026@gmail.com>
2025-12-18 16:33:13 +0530

    updated backend

--- Application/CategoryServices/category_models.py
+++ Application/CategoryServices/category_models.py
@@ -9,7 +9,8 @@ class Category(models.Model):
     description = models.TextField(blank=True, null=True)
     priority = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='category_images/', blank=True, null=True)
-    display_name= models.CharField(max_length=255, blank=True, null=True)
+    display_name1= models.CharField(max_length=255, blank=True, null=True)
+    display_name2= models.CharField(max_length=255, blank=True, null=True)
     
     is_available = models.BooleanField(default=True)
     special_tags = models.CharField(max_length=255, blank=True, null=True)
